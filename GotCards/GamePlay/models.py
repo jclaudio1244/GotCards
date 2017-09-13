f23cae4cbf132fa702744c5e8d412f23f1352a3e
Jael Claudio <jnc8951@rit.edu>
2017-09-13 01:38:20 -0400

    Created CardGame model and admin account.

--- GotCards/GamePlay/models.py
+++ GotCards/GamePlay/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class CardGame(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
